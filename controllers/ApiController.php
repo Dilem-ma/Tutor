ae4db8c544e27e9bc5741754410f890fc359935c
Vam <308170166@qq.com>
2017-03-16 17:10:22 +0800

    add table student

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -9,6 +9,7 @@
 namespace app\controllers;
 
 use app\models\LoginForm;
+use app\models\Student;
 use app\models\User;
 use yii\rest\Controller;
 
@@ -46,6 +47,48 @@ class ApiController extends Controller
                 'success' => false,
                 'message' => '注册失败',
                 'errors' => $user->getFirstError('username'),
+            ];
+        }
+    }
+
+    public function actionChange(){
+
+        $user = User::findOne(['username' => \Yii::$app->request->post('username')]);
+        $user->password = \Yii::$app->request->post('password');
+
+        if ($user->save()) {
+            return [
+                'success' => true,
+                'message' => '修改成功',
+                'username' => $user,
+            ];
+        } else {
+            return [
+                'success' => false,
+                'message' => '修改失败',
+                'errors' => $user->getFirstError('username'),
+            ];
+        }
+    }
+
+    public function actionIdentity(){
+        $stu = new Student();
+        $stu->load(\Yii::$app->request->post(), '');
+        $user = User::findByUsername(\Yii::$app->request->post('phone'));
+        $stu->u_id = $user->getId();
+
+        if ($stu->save()) {
+            return [
+                'success' => true,
+                'message' => '操作成功',
+            ];
+        } else {
+            return [
+                'success' => false,
+                'message' => '操作失败',
+                'errors' => $stu->errors,
+                'user' => $user,
+                'stu' => $stu,
             ];
         }
     }
