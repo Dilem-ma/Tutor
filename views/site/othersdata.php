1401342c693d79569d807210db26141d11a40baf
sjl872964789 <872964789@qq.com>
2017-10-25 18:35:20 +0800

    update index add teachersearch update mine

---
+++ views/site/othersdata.php
@@ -0,0 +1,96 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'OthersdataPage';
+\app\assets\OthersdataAsset::register($this);
+?>
+<div class="scTop">
+    <span class="title">个人资料</span>
+    <a href="mine"><img src="/img/back.png" class="back"/></a>
+</div>
+<div class="pane2">
+    <div class="pane2-1">
+        <div class="pane2-4">
+            <div class="pane2-5">
+                <div class="text6">
+                    <span class="text4">头像</span>
+                </div>
+                <div class="image1">
+                    <img align="absmiddle" src="/img/portrait2.png" class="pic_icon" height="70" width="70"/>
+                </div>
+            </div>
+        </div>
+        <div class="pane2-2">
+            <div class="pane2-3">
+                <div class="text3">
+                    <span class="text4">昵称</span>
+                </div>
+                <div class="text5">
+                    <span class="text4">小明</span>
+                </div>
+            </div>
+        </div>
+        <div class="pane2-2">
+            <div class="pane2-3">
+                <div class="text3">
+                    <span class="text4">性别</span>
+                </div>
+                <div class="text5">
+                    <span class="text4">男</span>
+                </div>
+            </div>
+        </div>
+        <div class="pane2-2">
+            <div class="pane2-3">
+                <div class="text3">
+                    <span class="text4">星级</span>
+                </div>
+                <div class="text5">
+                    <span class="text4">三星</span>
+                </div>
+            </div>
+        </div>
+        <div class="pane2-2">
+            <div class="pane2-3">
+                <div class="text3">
+                    <span class="text4">教育等级</span>
+                </div>
+                <div class="text5">
+                    <span class="text4">高中</span>
+                </div>
+            </div>
+        </div>
+        <div class="pane2-2">
+            <div class="pane2-3">
+                <div class="text3">
+                    <span class="text4">擅长科目</span>
+                </div>
+                <div class="text5">
+                    <span class="text4">Java</span>
+                </div>
+            </div>
+        </div>
+        <div class="pane2-2">
+            <div class="pane2-3">
+                <div class="text3">
+                    <span class="text4">个人描述</span>
+                </div>
+                <div class="text5">
+                    <span class="text4">cool</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="pane2-1">
+        <a href="talk">
+            <div class="pane2-2">
+                <div class="pane2-3">
+                    <div class="text3">
+                        <span class="text4">给他发消息</span>
+                    </div>
+                </div>
+            </div>
+        </a>
+    </div>
+</div>
