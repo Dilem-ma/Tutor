f4a690de52890619a62d9c71dc953a02f9a904e1
yanjing <1319831165@qq.com>
2017-10-28 17:20:20 +0800

    teacher_list

---
+++ views/site/teacherdata.php
@@ -0,0 +1,94 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'TeacherdataPage';
+\app\assets\PersonaldataAsset::register($this);
+?>
+<div class="scTop" >
+    <span class="title">教师资料</span>
+    <a href="mine"><img src="/img/back.png" class="back"/></a>
+</div >
+<div class="pane2" ng-controller="TeacherCtrl">
+    <div class="pane2-1">
+        <div class="pane2-4">
+            <div class="pane2-5">
+                <div class="text6">
+                    <span class="text4">头像</span>
+                </div>
+                <div class="image1">
+                    <img align="absmiddle" src="{{current_url}}" class="pic_icon" height="70" width="70"/>
+                </div>
+            </div>
+        </div>
+        <div class="pane2-2">
+            <div class="pane2-3">
+                <div class="text3">
+                    <span class="text4">昵称</span>
+                </div>
+                <div class="text5">
+                    <span class="text4">{{current_name}}</span>
+                </div>
+            </div>
+        </div>
+        <div class="pane2-2">
+            <div class="pane2-3">
+                <div class="text3">
+                    <span class="text4">性别</span>
+                </div>
+                <div class="text5">
+                    <span class="text4">{{current_gender}}</span>
+                </div>
+            </div>
+        </div>
+        <div class="pane2-2">
+            <div class="pane2-3">
+                <div class="text3">
+                    <span class="text4">手机号</span>
+                </div>
+                <div class="text5">
+                    <span class="text4">{{current_num}}</span>
+                </div>
+            </div>
+        </div>
+        <div class="pane2-2">
+            <div class="pane2-3">
+                <div class="text3">
+                    <span class="text4">地区</span>
+                </div>
+                <div class="text5">
+                    <span class="text4">{{current_area}}</span>
+                </div>
+            </div>
+        </div>
+        <div class="pane2-2">
+            <div class="pane2-3">
+                <div class="text3">
+                    <span class="text4">生日</span>
+                </div>
+                <div class="text5">
+                    <span class="text4">{{current_birthday}}</span>
+                </div>
+            </div>
+        </div>
+        <div class="pane2-2">
+            <div class="pane2-3">
+                <div class="text3">
+                    <span class="text4">擅长领域</span>
+                </div>
+                <div class="text5">
+                    <span class="text4">Java，c++</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="pane2-1">
+        <div class="pane2-2">
+            <div class="pane2-3">
+                <div class="text3">
+                    <span class="text4">修改密码</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ web/js/controllers/TeacherCtrl.js
@@ -0,0 +1,77 @@
+/**
+ * Created by Dilemma丶 on 2017/3/22.
+ */
+
+var storage, tutorApp;
+
+tutorApp = angular.module('tutorApp', []);
+
+storage = window.localStorage;
+
+tutorApp.controller('TeacherCtrl', function ($scope, $http) {
+    if (localStorage.getItem(storage) !== void 0) {
+        $scope.token = localStorage.getItem(storage);
+        console.log($scope.token);
+    } else {
+        $scope.token = void 0;
+    }
+    var q = {
+        method: 'get',
+        url: '/api/get_current_user',
+        params: {
+            'token': $scope.token
+        }
+    };
+    $http(q).then(function (d) {
+        $scope.current_url = d.data.url;
+        $scope.current_num = d.data.username;
+        $scope.current_name = d.data.name;
+        $scope.current_area = d.data.area;
+        if(d.data.gender=="male")
+            $scope.current_gender="男";
+        else
+            $scope.current_gender="女";
+        if(d.data.birthday==null)
+            $scope.current_birthday=" ";
+        else
+            $scope.current_birthday=d.data.birthday;
+    });
+    var p = {
+        method: 'get',
+        url: '/api/get_identity',
+        params: {
+            'token': $scope.token
+        }
+    };
+    $http(p).then(function (d) {
+        if (d.data.error != undefined) { //都不是
+            $scope.stu_status = 0; //没注册
+            $scope.tea_status = 0;
+        }
+        else if (d.data.teacher.id == undefined) { //不是老师是学生
+            $scope.tea_status = 0;
+            $scope.stu_status = 1;
+            $scope.stu_description = d.data.student.describe;
+            $scope.stu_grade = d.data.student.grade;
+        }
+        else if (d.data.student.id == undefined) { //不是学生是老师
+            $scope.tea_status = 1;
+            $scope.stu_status = 0;
+            $scope.tea_description = d.data.teacher.describe;
+            $scope.tea_education = d.data.teacher.education;
+            $scope.tea_major = d.data.teacher.major;
+            $scope.tea_star = d.data.teacher.star;
+        }
+        else {
+            $scope.tea_status = 1;
+            $scope.stu_status = 1;
+            $scope.stu_description = d.data.student.describe;
+            $scope.stu_grade = d.data.student.grade;
+            $scope.tea_description = d.data.teacher.describe;
+            $scope.tea_education = d.data.teacher.education;
+            $scope.tea_major = d.data.teacher.major;
+            $scope.tea_star = d.data.teacher.star;
+        }
+    });
+
+});
