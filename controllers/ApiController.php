8c6aaeaf5bfae304f0bbb4d05abebd7e95905b4a
VamZero <308170166@qq.com>
2017-11-07 20:34:09 +0800

    add get_stu_data api

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -17,6 +17,7 @@ use app\actions\GetCommentsAction;
 use app\actions\GetCurrentOrderAction;
 use app\actions\GetMyFavourite;
 use app\actions\GetNotPickedOrdersAction;
+use app\actions\GetStuDataAction;
 use app\actions\GetTeacherDataAction;
 use app\actions\GetUserDataAction;
 use app\actions\GetUserOrdersAction;
@@ -63,7 +64,7 @@ class ApiController extends Controller
                 ],
                 [
                     'allow' => true,
-                    'actions' => ['register', 'change_password', 'get_user_orders', 'get_teacher_data', 'get_current_order', 'delete_order', 'get_favourite', 'add_to_favourite', 'pick_up_order', 'stu_check_order', 'add_comment', 'get_comments', 'complete_order', 'fast_login', ],
+                    'actions' => ['register', 'change_password', 'get_user_orders', 'get_teacher_data', 'get_current_order', 'delete_order', 'get_favourite', 'add_to_favourite', 'pick_up_order', 'stu_check_order', 'add_comment', 'get_comments', 'complete_order', 'fast_login', 'get_stu_data', ],
                     'verbs' => ['POST'],
                 ],
 //                [
@@ -116,6 +117,7 @@ class ApiController extends Controller
             'get_comments' => GetCommentsAction::className(),
             'complete_order' => CompleteOrderAction::className(),
             'fast_login' => FastLoginAction::className(),
+            'get_stu_data' => GetStuDataAction::className(),
         ];
     }
 
